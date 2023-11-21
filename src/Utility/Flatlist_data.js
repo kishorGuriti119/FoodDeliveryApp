449e8ccc1a19bec59e5751ca500779b32406692a
kishor119 <kishior.guriti@motivitylabs.com>
2023-11-21 13:04:05 +0530

    flat list data

--- src/Utility/Flatlist_data.js
+++ src/Utility/Flatlist_data.js
@@ -41,3 +41,42 @@ export const Order_status = [
     icon: '',
   },
 ];
+
+export const FoodItems = [
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Efo Riro & Pounded Yam',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Egg Ramen',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Catfish Pepersoup',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Crayfish Ramen',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Chicken Alfredo',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+  {
+    itemImage: require('../assets/item_1.png'),
+    title: 'Chicken Alfredo',
+    price: 'N4,000',
+    deliveryTime: 30,
+  },
+];
