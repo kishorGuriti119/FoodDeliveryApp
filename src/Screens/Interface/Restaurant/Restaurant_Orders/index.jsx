5a1243e4905ddf483e0aa66b1ecbae7278da2b04
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-12-04 11:49:09 +0530

    logout bug fix

--- src/Screens/Interface/Restaurant/Restaurant_Orders/index.jsx
+++ src/Screens/Interface/Restaurant/Restaurant_Orders/index.jsx
@@ -1,95 +1,95 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, Image} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Button from '../../../../Components/Button';
-import {launchImageLibrary} from 'react-native-image-picker';
+import { launchImageLibrary } from 'react-native-image-picker';
 import RNFS from 'react-native-fs';
 import axios from 'axios';
 
 
-const Restaurant_Orders = () => {
-  const onLogut = async () => {
-    let token = await AsyncStorage.getItem('token');
-    if (token) {
-      console.log('token from backend');
-      await AsyncStorage.removeItem('token');
-      navigation.navigate('login');
-    } else {
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
-      console.log('log out');
-    }
-    navigation.navigate('login');
-  };
+const Restaurant_Orders = ({ navigation }) => {
+  // const onLogut = async () => {
+  //   let token = await AsyncStorage.getItem('token');
+  //   if (token) {
+  //     console.log('token from backend');
+  //     await AsyncStorage.removeItem('token');
+  //     navigation.navigate('login');
+  //   } else {
+  //     await GoogleSignin.revokeAccess();
+  //     await GoogleSignin.signOut();
+  //     console.log('log out');
+  //   }
+  //   navigation.navigate('login');
+  // };
 
-  const [token, setToken] = useState('');
-  const [preview, setPreview] = useState(null);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  // const [restuarantID, setRestuarantID] = useState(
-  //   '44487ffd-cd6b-4ab1-9d7f-90529de99a4f',
-  // );
+  // const [token, setToken] = useState('');
+  // const [preview, setPreview] = useState(null);
+  // const [selectedPhoto, setSelectedPhoto] = useState(null);
+  // // const [restuarantID, setRestuarantID] = useState(
+  // //   '44487ffd-cd6b-4ab1-9d7f-90529de99a4f',
+  // // );
 
-  const createFormData = (selectedPhoto, body = {}) => {
-    const data = new FormData();
+  // const createFormData = (selectedPhoto, body = {}) => {
+  //   const data = new FormData();
 
-    data.append('image', {
-      name: selectedPhoto.fileName,
-      type: selectedPhoto.type,
-      uri:
-        Platform.OS === 'ios'
-          ? selectedPhoto.uri.replace('file://', '')
-          : selectedPhoto.uri,
-    });
+  //   data.append('image', {
+  //     name: selectedPhoto.fileName,
+  //     type: selectedPhoto.type,
+  //     uri:
+  //       Platform.OS === 'ios'
+  //         ? selectedPhoto.uri.replace('file://', '')
+  //         : selectedPhoto.uri,
+  //   });
 
-    Object.keys(body).forEach(key => {
-      data.append(key, body[key]);
-    });
+  //   Object.keys(body).forEach(key => {
+  //     data.append(key, body[key]);
+  //   });
 
-    console.log(data);
-    return data;
-  };
+  //   console.log(data);
+  //   return data;
+  // };
 
-  useEffect(() => {
-    const getToken = async () => {
-      let Storedtoken = await AsyncStorage.getItem('token');
-      if (Storedtoken !== null) {
-        setToken(Storedtoken);
-      }
-    };
-    getToken();
-  }, [token]);
+  // useEffect(() => {
+  //   const getToken = async () => {
+  //     let Storedtoken = await AsyncStorage.getItem('token');
+  //     if (Storedtoken !== null) {
+  //       setToken(Storedtoken);
+  //     }
+  //   };
+  //   getToken();
+  // }, [token]);
 
-  const uploadImage = async () => {
-    fetch(`http://10.0.2.2:8082/api/restaurant/menu`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: createFormData(selectedPhoto, {
-        restaurantId: '44487ffd-cd6b-4ab1-9d7f-90529de99a4f',
-        name: 'chicken',
-        description: 'most popular food these days',
-        price: '290',
-      }),
-    })
-      .then(response => response.json())
-      .then(response => {
-        console.log('response', response);
-      })
-      .catch(error => {
-        console.log('error', error);
-      });
-  };
+  // const uploadImage = async () => {
+  //   fetch(`http://10.0.2.2:8082/api/restaurant/menu`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'multipart/form-data',
+  //     },
+  //     body: createFormData(selectedPhoto, {
+  //       restaurantId: '44487ffd-cd6b-4ab1-9d7f-90529de99a4f',
+  //       name: 'chicken',
+  //       description: 'most popular food these days',
+  //       price: '290',
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log('response', response);
+  //     })
+  //     .catch(error => {
+  //       console.log('error', error);
+  //     });
+  // };
 
-  const choosePhoto = async () => {
-    let response = await launchImageLibrary();
-    setSelectedPhoto(response);
-    setPreview(response.assets[0].uri);
-  };
+  // const choosePhoto = async () => {
+  //   let response = await launchImageLibrary();
+  //   setSelectedPhoto(response);
+  //   setPreview(response.assets[0].uri);
+  // };
 
   return (
     <View>
-      <Button title="uploadImage" onPress={uploadImage} />
+      {/* <Button title="uploadImage" onPress={uploadImage} />
       <Button title="logout" onPress={onLogut} />
       <Button title="choose" onPress={choosePhoto} />
       {preview && (
@@ -97,9 +97,9 @@ const Restaurant_Orders = () => {
           source={{
             uri: preview,
           }}
-          style={{height: 200, width: 200}}
+          style={{ height: 200, width: 200 }}
         />
-      )}
+      )} */}
     </View>
   );
 };
