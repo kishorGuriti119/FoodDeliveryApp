4b5ffefc39f1040ecdaddad63e9747407ebadaca
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-12-07 17:44:58 +0530

    Integrated pushnotifications in cloudmessaging

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigations from './src/Screens/Authenticate/SecureNavigations/navigations';
 import { Provider } from 'react-redux'
 import { store } from './src/Store/Store';
 import { PaperProvider } from 'react-native-paper';
+import { requestUserPermission,NotificationServices } from './src/Components/FireStore/FCM/PushNotifications';
 
 const App = () => {
-
-
+  useEffect(() => {
+    requestUserPermission()
+    NotificationServices()
+  })
   return (
     <SafeAreaProvider>
       <Provider store={store}>
